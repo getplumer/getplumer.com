d9ee350151593084c7f9700f9c6923f7a9fe7f1f
Nick Quinlan <nicholas.quinlan@sendgrid.com>
2014-03-02 15:07:56 -0800

    styled

--- js/main.js
+++ js/main.js
@@ -1,1 +1,5 @@
-
+$(".sendgrid-subscription-widget").on("success", function (e) {
+    $("body").addClass("subscribed");
+    $(this).find(".response")
+    // $(this).find(".response").addClass("success")
+});
