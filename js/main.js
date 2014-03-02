800bbf1b8a06b4f26d4772838e469ce58eedeacf
Nick Quinlan <nicholas.quinlan@sendgrid.com>
2014-03-02 15:37:26 -0800

    wrapped in function

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-$(".sendgrid-subscription-widget").on("success", function (e) {
-    $("body").addClass("subscribed");
-    $(this).find(".response")
-    // $(this).find(".response").addClass("success")
+$(function () {
+	$(".sendgrid-subscription-widget").on("success", function (e) {
+	    $("body").addClass("subscribed");
+	});
 });
