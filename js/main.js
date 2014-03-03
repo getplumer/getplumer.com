0d4b57dc63b5763a6236950e80fa41e7296d2253
Nick Quinlan <nicholas.quinlan@sendgrid.com>
2014-03-02 17:10:34 -0800

    added google analytics

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 $(function () {
 	$(".sendgrid-subscription-widget").on("success", function (e) {
 	    $("body").addClass("subscribed");
+	    _gaq.push(['_trackEvent', 'Form', 'Signup', 'Success']);
 	});
 
 	$.konami(function() {
