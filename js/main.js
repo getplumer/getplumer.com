87160b4dc29cbfe9f4286034eeb07cd55fc8f4e6
Nick Quinlan <nicholas.quinlan@sendgrid.com>
2014-03-02 17:08:06 -0800

    konami code
    this was a good use of time

--- js/main.js
+++ js/main.js
@@ -2,4 +2,8 @@ $(function () {
 	$(".sendgrid-subscription-widget").on("success", function (e) {
 	    $("body").addClass("subscribed");
 	});
+
+	$.konami(function() {
+		$("body").addClass("rockin");
+	});
 });
